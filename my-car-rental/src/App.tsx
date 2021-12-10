c3cc11bf5da60a7acf95143f60e7d420dd6f5d6e
Silviu Ifrim <silviu.redent@gmail.com>
2021-12-10 21:22:33 +0200

    Add links to sign up and login

--- my-car-rental/src/App.tsx
+++ my-car-rental/src/App.tsx
@@ -7,8 +7,13 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import { Link } from "@mui/material";
 
 export default function App() {
+
+  const loginUrl = "https://rent-my-car.auth.us-east-2.amazoncognito.com/login?client_id=5298aon6j1e7vjgnl3btv0ei5m&response_type=token&scope=email+openid+phone+profile&redirect_uri=https://rent-my-car.me/";
+  const registerUrl = "https://rent-my-car.auth.us-east-2.amazoncognito.com/signup?client_id=5298aon6j1e7vjgnl3btv0ei5m&response_type=token&scope=email+openid+phone+profile&redirect_uri=https://rent-my-car.me/";
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -20,18 +25,27 @@ export default function App() {
             <Typography variant="h6">Pricing</Typography>
           </Stack>
           <div>
-            <IconButton
-              size="large"
+            <Link
+              href={loginUrl}
               color="inherit"
             >
-              <AccountCircle />
-            </IconButton>
-            <IconButton
-              size="large"
-              color="inherit"
-            >
-              <PersonAddIcon />
-            </IconButton>
+              <IconButton
+                size="large"
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Link>
+            <Link
+              href={registerUrl}
+              color="inherit">
+              <IconButton
+                size="large"
+                color="inherit"
+              >
+                <PersonAddIcon />
+              </IconButton>
+            </Link>
           </div>
         </Toolbar>
       </AppBar>
