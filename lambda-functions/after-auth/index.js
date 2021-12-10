57285f103e0c5ffe581a82436b89dab4ef4808b0
Silviu Ifrim <silviu.redent@gmail.com>
2021-12-10 19:54:12 +0200

    Correct mutation query

--- lambda-functions/after-auth/index.js
+++ lambda-functions/after-auth/index.js
@@ -1,23 +1,26 @@
 const request = require('request');
 
-exports.handler =  (event, context, callback) => {
-  const userId = event.userName;
-  const hasuraAdminSecret = process.env.HASURA_ADMIN_SECRET;
-  const url = process.env.URL;
-  const upsertUserQuery = `
-    mutation($userId: String!){
-      insert_users(objects: [{ id: $userId }], on_conflict: { constraint: users_pkey, update_columns: [] }) {
-        affected_rows
-      }
-    }`
-  const graphqlReq = { "query": upsertUserQuery, "variables": { "userId": userId } }
+exports.handler = (event, context, callback) => {
+    const userId = event.userName;
+    const hasuraAdminSecret = process.env.HASURA_ADMIN_SECRET;
+    const url = process.env.URL;
+    const upsertUserQuery = `
+            mutation MyMutation {
+                insert_users_user(objects: {id: "${userId}"}) {
+                returning {
+                    id
+                }
+            }
+        }`;
 
-  request.post({
-      headers: {'content-type' : 'application/json', 'x-hasura-admin-secret': hasuraAdminSecret},
-      url:   url,
-      body:  JSON.stringify(graphqlReq)
-  }, function(error, response, body){
-       console.log(body);
-       callback(null, user, context);
-  });
+    const graphqlReq = { "query": upsertUserQuery };
+
+    request.post({
+        headers: { 'content-type': 'application/json', 'x-hasura-admin-secret': hasuraAdminSecret },
+        url: url,
+        body: JSON.stringify(graphqlReq)
+    }, function (error, response, body) {
+        console.log(body);
+        callback(null, user, context);
+    });
 }
