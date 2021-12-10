4701cea60feb47db935e18fdd979b48150eaa786
Ilie Cristian <48507583+iliecristian@users.noreply.github.com>
2021-12-11 01:57:28 +0200

    leaflet setup

--- my-car-rental/src/app/components/Content.js
+++ my-car-rental/src/app/components/Content.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import App from '../../App';
 import Users from '../../components/users/User';
+import RentCarPage from '../pages/rent-car-page/RentCarPage';
 
 export default function Content() {
     return (
         <Routes>
-            <Route path="/" element={<App/>}/>
-            <Route path="/rent-car" element={"Rent a car"}/>
-            <Route path="/lend-car" element={"Lend a car"}/>
-            <Route path="/users" element={<Users/>}/>
+            <Route path="/" element={<App />} />
+            <Route path="/rent-car" element={<RentCarPage />} />
+            <Route path="/lend-car" element={"Lend a car"} />
+            <Route path="/users" element={<Users />} />
         </Routes>
     );
 }
---
+++ my-car-rental/src/app/pages/reviews-page/ReviewsPage.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+
+export const StyledDiv = styled.div`
+    color: ${props => props.green || 'red'};
+`
