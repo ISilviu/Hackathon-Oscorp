bc9eb103662f6042852295293bab0a33a7986c21
Ilie Cristian <48507583+iliecristian@users.noreply.github.com>
2021-12-11 12:37:36 +0200

    Changd zoom level

--- src/app/components/car-map/CarMap.js
+++ src/app/components/car-map/CarMap.js
@@ -4,11 +4,6 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { StyledContainer, StyledMapContainer } from "./CarMap.styles"
 
 const CarMap = ({ width, height, children, center }) => {
-    const [map, setMap] = useState(null)
-
-    if (map && center) {
-        map.flyTo(center);
-    }
 
     return (
         <StyledContainer>
@@ -17,9 +12,9 @@ const CarMap = ({ width, height, children, center }) => {
                     style={{
                         width: width, height: height,
                     }}
-                    center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                    whenCreated={setMap}
-                    center={center}
+                    center={[45.663306, 24.866314]}
+                    zoom={7}
+                >
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
