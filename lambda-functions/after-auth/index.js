9b55448578546b4b90d9cd226e76fdebe855bcb4
Silviu Ifrim <silviu.redent@gmail.com>
2021-12-11 04:32:08 +0200

    Add user profile, remove updated at

--- lambda-functions/after-auth/index.js
+++ lambda-functions/after-auth/index.js
@@ -6,13 +6,19 @@ exports.handler = (event, context, callback) => {
     const username = event.userName;
     const userEmail = event.request.userAttributes.email;
     const userPicture = event.request.userAttributes.picture;
-    const updatedAt = event.request.userAttributes.updated_at;
+    const userProfile = event.request.userAttributes.updated_at;
+
+    const userRoleId = userProfile === 'lender' ? 'c7a906bb-3b4a-489b-8eb1-0b745a3f0112' : '124504a4-02ed-4c81-bf13-84da9be08ad8';
 
     const hasuraAdminSecret = process.env.HASURA_ADMIN_SECRET;
     const url = process.env.URL;
     const upsertUserQuery = `
             mutation MyMutation {
-                insert_users_user(objects: {id: "${userId}", username: "${username}", name: "${userName}", email: "${userEmail}", picture: "${userPicture}", updated_at:"${updatedAt}"}) {
+                insert_users_user(objects: {id: "${userId}", username: "${username}", name: "${userName}", email: "${userEmail}", picture: "${userPicture}"}) {
+                returning {
+                    id
+                }
+                insert_users_user_roles(objects: {role_id: "${userRoleId}", user_id: "${userId}"}) {
                 returning {
                     id
                 }
